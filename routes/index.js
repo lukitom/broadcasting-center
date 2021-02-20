b0f4d0d06d7a26fdbb32ef4e0f039bb92c11565b
Łukasz Tomaszek <tomaszek.l03@gmail.com>
2021-02-20 18:32:51 +0100

    change files from ended css view

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/',  (req, res) => {
+    res.render('index');
+});
+
+router.get('/index', (req, res) => {
     res.render('index');
 });
 
