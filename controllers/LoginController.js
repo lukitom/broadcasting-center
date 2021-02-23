b4a33c9f02c8f53d3d2b274edc7591c81444ade6
Łukasz Tomaszek <tomaszek.l03@gmail.com>
2021-02-23 16:37:08 +0100

    add bcrypt create implementation for RegisterController, LoginController

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,4 +1,23 @@
 exports.loginSystem = (req, res) => {
+    const bcrypt = require('bcrypt');
+    const database = require('../database/ConnectionDatabase');
+
+    const sql = `SELECT * FROM users where login="${req.body.loginLog}"`;
+    database.query(sql, async (err, result) => {
+        if (err) throw err;
+        console.log(result)
+
+        try {
+            if (await bcrypt.compare(req.body.haslo, result[0].password)) {
+                res.send('Success');
+            } else {
+                res.send('Not Allowed');
+            }
+        } catch {
+            res.status(500).send();
+        }
+    });
+
     console.log(`Wysłano formularz do logowania z wartościami '${req.body.loginLog}' jako login i '${req.body.haslo}' jako hasło.`);
-    res.redirect('/index')
+    res.redirect('/suggestion-song')
 };
--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,4 +1,19 @@
-exports.registerSystem = (req, res) => {
+exports.registerSystem = async (req, res) => {
+    const bcrypt = require('bcrypt');
+    const database = require('../database/ConnectionDatabase');
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.haslo, 10);
+        console.log(hashedPassword);
+        const sql = `INSERT INTO users (login, password, permission) values ("${req.body.loginRegister}", "${hashedPassword}", ${2})`;
+        database.query(sql, (err) => {
+            if (err) throw err;
+            res.status(201);
+        })
+    } catch {
+
+    }
+
     console.log(`Przesłano formularz rejestracji, a w nim podano login: ${req.body.loginRegister}, email: ${req.body.email} oraz hasło: ${req.body.haslo}`);
-    res.redirect('/index');
+    // res.redirect('/index');
 }
