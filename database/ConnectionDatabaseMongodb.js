8ea8a2e66f161215aa565a64c256f00170f020b1
Łukasz Tomaszek <tomaszek.l03@gmail.com>
2021-03-10 09:01:32 +0100

    registration and registration system with passport create .env add eslint end nav bar template in suggestionSong, voteSong and protect unAuth

--- database/ConnectionDatabaseMongodb.js
+++ database/ConnectionDatabaseMongodb.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
+const user = process.env.MONGODB_USER
+const pass = process.env.MONGODB_PASS
 
-var uri = "mongodb_connection_string";
+const uri2 = "mongodb+srv://" + user + ":" + pass + "@cluster0.sugax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {
+mongoose.connect(uri2, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
 })
-    .then(() => { })
+    .then(() => { console.log("MongoDb connected ...")})
     .catch((err) => { console.log(`${err}`)});
 
 module.exports = mongoose.connection;
