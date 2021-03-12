133fa13b238eed9413622ff0518f81dce2542490
jakur0 <jakur0@op.pl>
2021-03-12 16:26:33 +0100

    try fetch full playlist without explicit

--- controllers/SpotifyTrackController.js
+++ controllers/SpotifyTrackController.js
@@ -1,3 +1,4 @@
+const { mongo } = require('mongoose');
 const request = require('request'); // "Request" library
 const TrackModel = require('../database/models/TrackModel');
 
@@ -19,44 +20,116 @@ exports.test = (req, res) => {
     };
 
     request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
+        if (!error && response.statusCode === 200) {
 
-        // use the access token to access the Spotify Web API
-        var token = body.access_token;
-        //console.log(token);
-        var options = {
-        url: 'https://api.spotify.com/v1/tracks/4MzXwWMhyBbmu6hOcLVD49;',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        },
-        json: true
-        };
-        request.get(options, function(error, response, body) {
-        //res.status(200).json(body);
-        let temp_artist =[];
-        body.artists.forEach(element => {
-            temp_artist.push(element.name);
-        });
-        const newTrack = new TrackModel({
-            title: body.name,
-            artist: temp_artist,
-            _id: body.id,
-            prewiewURL: body.preview_url,
-            uri: body.uri
-        });
-        console.log(newTrack);
+            // use the access token to access the Spotify Web API
+            var token = body.access_token;
+            console.log(token);
+            var options = {
+            //url: 'https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?market=PL',
+            url: 'https://api.spotify.com/v1/tracks/6tDDoYIxWvMLTdKpjFkc1B?market=PL',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+            json: true
+            };
+            request.get(options, function(error, response, body) {
+            //res.status(200).json(body);
 
-        newTrack.save()
-            .then(() => {
-                // req.flash('success_msg', 'Rejestracja się powiodła. Możesz się zalogować.')
-                res.redirect('/');
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+            //#region wiele utworów - playlista 
+        //     let tracks=[];
+        //     for(i = 0; i < body.total; i++){
+        //         if (body.items[i].track.explicit) continue
+        //         let temp_artist =[];
+        //         body.items[i].track.artists.forEach(element => {
+        //             temp_artist.push(element.name);
+        //         });
+        //         let temp_track ={
+        //             title: body.items[i].track.name,
+        //             artist: temp_artist,
+        //             track_id: body.items[i].track.id,
+        //             prewiewURL: body.items[i].track.preview_url,
+        //             uri: body.items[i].track.uri
+        //         };
+        //         tracks.push(temp_track);
 
-        res.redirect('/');
-        });
-    }
+        //         // TrackModel.findById(i, (err, res) => {
+        //         //     if(err) console.log(error);
+        //         //     if(!res) {
+        //         //         const newTrack = new TrackModel({
+        //         //             title: temp_track.title,
+        //         //             artist: temp_track.artist,
+        //         //             track_id: temp_track.track_id,
+        //         //             prewiewURL: temp_track.prewiewURL,
+        //         //             uri: temp_track.uri
+        //         //         });
+        //         //         newTrack.save()
+        //         //             .then(() => {
+        //         //                 console.log(newTrack.title)
+        //         //             })
+        //         //             .catch((err) => {
+        //         //                 console.log(err);
+        //         //             });
+        //         //     }
+
+        //         // })
+        //     }
+        //     console.log(tracks);
+        //     TrackModel.insertMany(tracks);
+        //     // tracks.forEach(track =>{
+        //     //     TrackModel.find({track_id: track.track_id}, (err, res) => {
+        //     //         if(err) console.log(error);
+        //     //         if(!res) {
+        //     //             const newTrack = new TrackModel({
+        //     //                 title: track.title,
+        //     //                 artist: track.artist,
+        //     //                 track_id: track.track_id,
+        //     //                 prewiewURL: track.prewiewURL,
+        //     //                 uri: track.uri
+        //     //             });
+        //     //             newTrack.save()
+        //     //                 .then(() => {
+        //     //                     console.log(newTrack.title)
+        //     //                 })
+        //     //                 .catch((err) => {
+        //     //                     console.log(err);
+        //     //                 });
+        //     //         }
+
+        //     //     })
+        //     // });
+
+        // // for(i =0; i< tracks;i++)
+
+        //#endregion
+
+
+
+
+            let temp_artist =[];
+            body.artists.forEach(element => {
+                temp_artist.push(element.name);
+            });
+
+            const newTrack = new TrackModel({
+                title: body.name,
+                artist: temp_artist,
+                _id: body.id,
+                prewiewURL: body.preview_url,
+                uri: body.uri
+            });
+            console.log(newTrack);
+
+            newTrack.save()
+                .then(() => {
+                    res.redirect('/');
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+
+            res.redirect('/');
+            });
+        }
     });
 };
