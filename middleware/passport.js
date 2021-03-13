237c6cbde1b64013e53ffb99c234583f050c19c1
Łukasz Tomaszek <tomaszek.l03@gmail.com>
2021-03-13 17:46:17 +0100

    add responce incorrect login data

--- middleware/passport.js
+++ middleware/passport.js
@@ -6,11 +6,11 @@ const UserModel = require('../database/models/UserModel');
 
 module.exports = function (passport) {
     passport.use(
-        new LocalStrategy({ usernameField: 'login' }, (login, password, done) => {
+        new LocalStrategy({ usernameField: 'login', passReqToCallback: true }, (req, login, password, done) => {
             // find user in database using login
             UserModel.findOne({ login: login })
                 .then(user => {
-                    if (!user) return done(null, false, { message: 'Niepoprawne dane' });
+                    if (!user) return done(null, false, req.flash('error', 'Niepoprawne dane'));
 
                     // sprawdzenie poprawnośći podadanego hasła
                     bcrypt.compare(password, user.password, (err, isMatch) => {
@@ -19,7 +19,7 @@ module.exports = function (passport) {
                         if (isMatch) {
                             return done(null, user);
                         } else {
-                            return done(null, false, { message: 'Niepoprawne dane' })
+                            return done(null, false, req.flash('error', 'Niepoprawne dane'))
                         }
                     });
                 })
