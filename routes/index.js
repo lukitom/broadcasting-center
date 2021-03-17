3a768d4f3eb9558b3ff0616594697ee14d00391e
Damian352 <damian.kulsza@gmail.com>
2021-03-17 10:16:54 +0100

    try vote with axios but failed

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,20 @@ router.get('/', PageConstroller.index);
 router.get('/index', PageConstroller.index);
 router.get('/login', canRegisterLogin, PageConstroller.login);
 router.get('/register', canRegisterLogin, PageConstroller.register);
-router.get('/suggestionSong', ensureAutheticated, PageConstroller.suggestionSong);
-router.get('/voteSong', ensureAutheticated, PageConstroller.voteSong);
+router.get('/suggestionSong',/* ensureAutheticated,*/ PageConstroller.suggestionSong);
+router.get('/voteSong',/* ensureAutheticated,*/ PageConstroller.voteSong);
 router.get('/logOut', PageConstroller.logOut);
-
+/*router.post('/test',(req, res) => {
+    console.log(req.params);
+    res.status(200).json(
+        {id: req.params}
+        
+    );
+});*/
+// router.post('/test', (req, res, next) => {
+//     //res.status(200).json(["Tony","Lisa","Michael","Ginger","Food"]);
+//     res.json({ username: 'Flavio' })
+//    });
 
 router.post('/login', canRegisterLogin, LoginConstroller.loginSystem);
 router.post('/register', canRegisterLogin, RegisterController.registerSystem);
