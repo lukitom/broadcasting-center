e191eca017f59ae60fa9f4d512b9cd2ba33c73c1
Łukasz Tomaszek <tomaszek.l03@gmail.com>
2021-04-03 14:35:57 +0200

    Merge commit '27adc1fa8f9a1cf4dbed3f7bfefbe52016d88ad5' into node_js

--- public/js/suggestion.js
+++ public/js/suggestion.js
@@ -1,34 +1,55 @@
-$(function(){
-    function wyswietl(data){
-        let div = $("#containerM");
-        // ! TODO: wstawić czyszczenie po ponownym wyszukiwaniu
-        // while (div.hasChildNodes) {
-        //     div.removeChild(div.firstChild);
-        // }
-        $(".resutlSearch").remove()
-        $("#textForm")[0].value = '';
-        data.forEach(function(element){
-            let p = document.createElement("p");
-            let a = document.createElement("a");
-            p.setAttribute("class", "resutlSearch");
-            a.setAttribute("class", "link");
-            a.setAttribute("target", "_blank");
-            a.setAttribute("href", element.prewiewURL);
-            a.innerHTML = "Posłuchaj fragmentu";
-            p.innerHTML = element.title + ' / ' + element.artist + " - ";
-            p.append(a);
-            div.append(p);
-        });
-    }
+$(function () {
+    function wyswietl(data, status) {
 
-    var przycisk = $(".przycisk");
-    przycisk.on("click", function(){
+        if ($("#containerM").children().length >= 1) {
+            $("#containerM").each(function () {
+                $(this).remove($(this).firstChild);
+            });
+            let divn = document.createElement("div");
+            divn.setAttribute("class", "containerMid");
+            divn.setAttribute("id", "containerM");
+            $("#main").append(divn);
+        }
+        let div = $("#containerM");
+
+        if (data.result != null) {
+            data.result.forEach(function (element) {
+                let p = document.createElement("p");
+                let btn = document.createElement("button");
+                p.setAttribute("class", "akapit");
+                p.innerHTML = element.title + ' / ' + element.artist;
+                if (element.prewiewURL != null) {
+                    let a = document.createElement("a");
+                    a.setAttribute("class", "link");
+                    a.setAttribute("href", element.prewiewURL);
+                    a.innerHTML = "Posłuchaj fragmentu";
+                    p.innerHTML += " - ";
+                    p.append(a);
+                }
+                div.append(p);
+            });
+        }
+        else {
+            let P = document.createElement("p");
+            P.setAttribute("class", "akapit");
+            P.innerHTML = "Brak piosenek";
+            div.append(P);
+        }
+    }
+    $(".przycisk").on("click", function () {
         const wartosc = $('#textForm')[0].value;
         const path = `/api/find?search=${wartosc}&type=track`;
 
         axios
             .get(path)
-            .then(res => wyswietl(res.data))
-            .catch(err => console.error(err));
+            .then(res => wyswietl(res.data, res.status))
+            .catch(err => {
+                let div = $("#containerM");
+                let P = document.createElement("p");
+                P.setAttribute("class", "akapit");
+                P.innerHTML = "Brak piosenek";
+                div.append(P);
+                console.error(err);
+            });
     });
 });
