8d448b86d30dfa74ad41cda19aed8c7cbaea00ba
lukitom <tomaszek.l03@gmail.com>
2022-08-09 12:19:38 +0200

    - correct 404 and 500 error displaying

--- app.js
+++ app.js
@@ -70,14 +70,12 @@ app.use('/', routesIndex);
 app.use('/api', api);
 app.use('/spotify', spotifyApi);
 
-// ! TODO: sprawdzić czy trzeba next
-app.use((req, res, next) => {
-    res.status(404).json({ error: "Not found" });
+app.use((req, res) => {
+    res.status(404).render('404');
 })
 
-// ! TODO: sprawdzić czy trzeba next
 app.use((err, req, res, next) => {
-    res.status(500).json({ error: err.message });
+    res.status(500).render('500');
 })
 
 module.exports = app;
